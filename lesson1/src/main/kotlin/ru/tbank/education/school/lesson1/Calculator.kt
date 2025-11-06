91d0a605379d0b83de4afbf071c2467056315668
cheburektop1 <Gregore40@yandex.ru>
2025-11-06 11:49:31 +0300

    все задачи урока

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -23,5 +23,24 @@ fun calculate(a: Double, b: Double, operation: OperationType): Double? {
  */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val resultArray = this.split(" ")
+    if (resultArray.size != 3) {
+        return (null)
+    } else {
+        var result2: Double = 0.0
+        when (resultArray[1]) {
+            "+" -> result2 = resultArray[0].toDouble() + resultArray[2].toDouble()
+            "-" -> result2 = resultArray[0].toDouble() - resultArray[2].toDouble()
+            "*" -> result2 = resultArray[0].toDouble() * resultArray[2].toDouble()
+            "/" -> {
+                if (resultArray[2].toDouble() != 0.0) {
+                    result2 = resultArray[0].toDouble() / resultArray[2].toDouble()
+                }
+            }
+        }
+        return result2
+    }
+}
+fun main(){
+    println("5 + 2".calculate())
 }
--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,9 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    println("iklp")
+    val a = 1
+    var n = 2
+    println("iklp;$a")
+    println(n + 1)
+    println(null)
 }
