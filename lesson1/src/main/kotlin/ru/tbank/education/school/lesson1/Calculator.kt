1a39cb1815842eec6cf2a05850727411d664d2a2
cheburektop1 <Gregore40@yandex.ru>
2025-11-06 10:17:54 +0300

    первая задача калькулятор

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,10 +1,19 @@
 package ru.tbank.education.school.lesson1
 
+import ru.tbank.education.school.lesson1.OperationType.*
+
 /**
  * Метод для вычисления простых арифметических операций.
  */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    var resultof: Double = 0.0
+    when(operation){
+        ADD -> resultof = a + b
+        SUBTRACT -> resultof = a - b
+        MULTIPLY -> resultof = a * b
+        DIVIDE -> resultof = a / b
+    }
+    return resultof
 }
 
 /**
