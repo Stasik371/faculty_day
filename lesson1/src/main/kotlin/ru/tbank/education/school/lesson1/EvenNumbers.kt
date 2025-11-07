ebddd853e78fae039afb974dcb2d2d363fa7ade5
Blacklow13 <aysel.baratova@mail.ru>
2025-11-07 11:45:10 +0300

    Revert "Первая версия калькулятора"
    This reverts commit 9048858497d02d0f62fd745338f43b317a3082ce.
    
    # Conflicts:
    #	lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -3,9 +3,6 @@ package ru.tbank.education.school.lesson1
 /**
  * Сумма четных чисел.
  */
-fun sumEvenNumbers(numbers: Array<Int>) = numbers.filter{it % 2 == 0}.sum()
-
-fun main(){
-    val array = arrayOf(1, 2, 3, 4, 6)
-    println(sumEvenNumbers(array))
+fun sumEvenNumbers(numbers: Array<Int>): Int {
+    TODO()
 }
