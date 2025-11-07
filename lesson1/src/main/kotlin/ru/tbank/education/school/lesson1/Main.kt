b6f731e2cfb3b90135f213ca8190c9cac1df61d7
Blacklow13 <aysel.baratova@mail.ru>
2025-11-07 11:41:16 +0300

    Revert "Добавили выводы задач первой практики!!!"
    This reverts commit 504c5be1d2818443e05c5db8d0fdf62494b84fb3.

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,12 +1,5 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    val a = 1
-    println("value of a = $a")
-    var b = 2
-    println("value of b = $b")
-    b++
-    println("value of b = $b")
-    val c: Short? = null
-    println("value of c = $c")
+    println("Enemy spotted ct");
 }
